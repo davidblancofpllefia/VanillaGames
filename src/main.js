e92aca5202495884a03a555375ec4f23d0fd447f
daw2-blanco24 <blancofernandezdavid@fpllefia.com>
2024-01-12 15:14:13 +0100

    Desplegando la aplicación en un servidor de pruebas (GitHub Pages)

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import { header } from "./componentes/header.js";
+import footer from "./componentes/footer.js";
+
+// Import all of Bootstrap's JS
+import 'bootstrap'
+
+// Import our custom CSS
+import './scss/style.scss'
+
+
+// Importamos la vista por defecto (que será home)
+async function cargarVista(){
+    const componente = await import('./vistas/homeVista')
+    const vista = componente.default
+    //Inyectamos la vista home
+    document.querySelector('main').innerHTML = vista.template
+}
+cargarVista()
+//Inyectamos el componente header
+document.querySelector('header').innerHTML = header.template
+
+//Inyectamos el componente footer
+document.querySelector('footer').innerHTML = footer.template
