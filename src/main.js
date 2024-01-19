2b18800c5f68d3649a760542562df59705596a9b
daw2-blanco24 <blancofernandezdavid@fpllefia.com>
2024-01-19 15:39:47 +0100

    Componente enrutador.js - Falta poner el login

--- src/main.js
+++ src/main.js
@@ -1,23 +1,21 @@
+// Import our custom CSS
+import './scss/styles.scss'
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// Importamos componentes header y footer
 import { header } from './componentes/header'
 import { footer } from './componentes/footer'
 
-import './scss/styles.scss'
+// Importamos la Función para detectar eventos al cargar las vistas
+import { enrutador } from './componentes/enrutador'
 
-// Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/registroVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-  // Ejecutamos la lógica de la vista
-  vista.script()
-}
-cargarVista()
-
-// Inyectamos el componente header
 document.querySelector('header').innerHTML = header.template
 
-// Inyectamos el componente footer
 document.querySelector('footer').innerHTML = footer.template
+
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
 
 
